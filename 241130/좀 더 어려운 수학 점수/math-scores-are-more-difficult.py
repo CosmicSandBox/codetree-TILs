0313d4113cf24cf1ef9bb671205e0e50e044bde4
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-11-30 23:13:27 +0900

    Title: 좀 더 어려운 수학 점수, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241130/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 241130/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,6 +1,7 @@
 A_math, A_eng = map(int,input().split())
 B_math, B_eng = map(int,input().split())
 
+""" # 쓰레기 코드
 if A_math == B_math :
     if A_eng > b_eng :
         print('A')
@@ -12,3 +13,12 @@ else :
         print('A')
     else :
         print('B')
+""" 
+
+# 그냥 수학 점수가 더 높으면 그냥 높은애 출력 (차이가 나면)
+# 수학 점수가 같다면 그땐 이제 영어를 비교
+
+if A_math > B_math or (A_math == B_math and A_eng > B_eng ) :
+    print('A')
+else :
+    print('B')
