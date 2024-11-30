07ee4903a4ec37cf34800a22ea6b2e80d78354c5
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-11-30 22:47:33 +0900

    Title: 장학금, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241130/장학금/scholarship.py
+++ 241130/장학금/scholarship.py
@@ -1,4 +1,4 @@
-mid, fin = map(int, input())
+mid, fin = map(int, input().split())
 
 if mid >= 90:
     if fin >= 95 :
@@ -7,3 +7,5 @@ if mid >= 90:
         print(5)
     else :
         print(0)
+else :
+    print(0)
