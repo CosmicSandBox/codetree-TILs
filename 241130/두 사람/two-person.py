aaefa03cafc471d4b9d27572baacc3cf1315f20d
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-11-30 23:28:09 +0900

    Title: 두 사람, Time: 82ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241130/두 사람/two-person.py
+++ 241130/두 사람/two-person.py
@@ -1,7 +1,7 @@
 A = input().split()
 B = input().split()
 
-if (int(A[0]) >= 19 or int(B[0]) >= 19) and (A[1] == M or A[0] == M):
+if (int(A[0]) >= 19 or int(B[0]) >= 19) and (A[1] == M or B[1] == M):
     print(1)
 else :
     print(0)
