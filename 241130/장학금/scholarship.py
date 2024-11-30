186064328a0ccb63d08057dc8de0d37a51d28f4b
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-11-30 23:17:20 +0900

    Title: 장학금, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241130/장학금/scholarship.py
+++ 241130/장학금/scholarship.py
@@ -1,5 +1,7 @@
 mid, fin = map(int, input().split())
 
+"""
+# 이렇게 하면 풀리기는 하는데 모범 답안은 아닌듯
 if mid >= 90:
     if fin >= 95 :
         print(100000)
@@ -7,5 +9,13 @@ if mid >= 90:
         print(50000)
     else :
         print(0)
+else :
+    print(0)
+"""
+
+if mid >= 90 and fin >= 95 :
+    print(100000)
+elif mid >= 90 and fin >= 90 :
+    print(50000)
 else :
     print(0)
