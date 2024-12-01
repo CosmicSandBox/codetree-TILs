9184b06045a9db0b817b8a5eba705c93c8b00dbe
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-01 19:27:58 +0900

    Title: 중앙값 구하기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 241201/중앙값 구하기/find-the-median.py
+++ 241201/중앙값 구하기/find-the-median.py
@@ -2,7 +2,7 @@ a, b, c = map(int, input().split())
 
 # a가 중앙값이라고 가정하거나  중앙값이 아니라고 가정해서 (케이스 2개) 구해보자
 
-if (a > b and a < c) or (a > c and a > c) :
+if (a > b and a < c) or (a > c and a < b) :
     print(a)
 else :
     if (b > c and a > b) or (c > b and b > a) :
