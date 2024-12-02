6ccd73b1b142e7b30135e0f37e48849f9a3b089d
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-02 18:36:59 +0900

    Title: 369 게임, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241202/369 게임/369-game.py
@@ -0,0 +1,14 @@
+n = int(input())
+# 1~n까지 369 게임을 진행한다 
+
+# 3의 배수 or 3,6,9 중 하나라도 들어가 있으면 0을 출력 그렇지 않으면
+
+for i in range (1, n+1):
+
+    if i % 3 == 0:
+        print(0, end=" ")
+    elif i % 10 == 3 or 6 or 9 :
+        print(i, end=" ")
+
+    else:
+        print(i, end=" ")
