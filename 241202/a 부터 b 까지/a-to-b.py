8afc230b8ee4608dcda636f757ca1c9d601ca99b
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-02 18:23:41 +0900

    Title: a 부터 b 까지, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241202/a 부터 b 까지/a-to-b.py
+++ 241202/a 부터 b 까지/a-to-b.py
@@ -1,9 +1,12 @@
 a, b = map(int,input().split())
 
+
 while a <= b :
     if a % 2 != 0 :
+        print(a, end=" ")
         a *= 2
-        print(a)
+        
     elif a % 2 == 0 :
+        print(a, end=" ")
         a += 3
-        print(a)
+        
