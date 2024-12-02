1f93898d7177ba915b4f2119e265bdb8abf49607
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-02 18:41:25 +0900

    Title: 369 게임, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241202/369 게임/369-game.py
+++ 241202/369 게임/369-game.py
@@ -5,10 +5,7 @@ n = int(input())
 
 for i in range (1, n+1):
 
-    if i % 3 == 0:
+    if i % 3 == 0 or i % 10 == 3 or 6 or 9 
         print(0, end=" ")
-    elif i % 10 == 3 or 6 or 9 :
-        print(i, end=" ")
-
     else:
         print(i, end=" ")
