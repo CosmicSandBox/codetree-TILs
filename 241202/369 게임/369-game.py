84a334d9df0ac1f9c7828d753bef44be779e7ee5
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-02 18:38:06 +0900

    Title: 369 게임, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241202/369 게임/369-game.py
+++ 241202/369 게임/369-game.py
@@ -8,7 +8,7 @@ for i in range (1, n+1):
     if i % 3 == 0:
         print(0, end=" ")
     elif i % 10 == 3 or 6 or 9 :
-        print(i, end=" ")
+        print(0, end=" ")
 
     else:
         print(i, end=" ")
