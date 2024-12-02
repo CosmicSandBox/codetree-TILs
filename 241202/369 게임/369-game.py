42c6db192272a36d25c05dd5ac1dfcb5d6b4b640
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-02 20:20:34 +0900

    Title: 369 게임, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241202/369 게임/369-game.py
+++ 241202/369 게임/369-game.py
@@ -4,8 +4,7 @@ n = int(input())
 # 3의 배수 or 3,6,9 중 하나라도 들어가 있으면 0을 출력 그렇지 않으면
 
 for i in range (1, n+1):
-
-    if i % 3 == 0 or (i % 10 == 3 or 6 or 9) :
+    if i % 3 == 0 or (i % 10 in [3, 6, 9]) :
         print(0, end=" ")
     else:
         print(i, end=" ")
