3843f62e84ff5e8ca1d18183ba4b6fbf48b32ae2
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-12 23:59:32 +0900

    Title: 범위 안의 수 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241212/범위 안의 수 2/number-in-range-2.py
+++ 241212/범위 안의 수 2/number-in-range-2.py
@@ -1,15 +1,12 @@
 sum_val = 0
 cnt = 0
 
-for i in range(10):
+for _ in range (10) :
     a = int(input())
-    if 0 <= a <= 200 :
+    if a >= 0 and a <= 200 :
         sum_val += a
         cnt += 1
 
-print(sum_val, end= " ")
+print(sum_val, end=" ")
 print(f"{sum_val/cnt:.1f}")
-
-
-        
 
