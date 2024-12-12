91aca1e45bdb2a4899487f1a749b70aa7f470199
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2024-12-12 14:22:09 +0900

    Title: 정수 사이의 배수, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241212/정수 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+sum_val = 0
+cnt = 0
+
+for i in range(a, b+1) :
+    if i % 7 == 0 or i % 5 == 0 :
+        sum_val += i
+        cnt += 1
+
+print(sum_val, end = " ")
+print(f"{sum_val/(b-a+1):.1f}")
+        
