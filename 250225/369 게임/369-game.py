a89fc9f1aebe7a0b14ad22b32697797af79d14a9
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-02-25 02:58:39 +0900

    Title: 369 게임, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250225/369 게임/369-game.py
+++ 250225/369 게임/369-game.py
@@ -2,6 +2,6 @@ N = int(input())
 
 for n in range (3, N+1, 3) :
     if (n % 3 == 0) or ((n - (n%10))%3 == 0) or ((n//10)%3 == 0) :
-        print(n)
-    else :
         print(0)
+    else :
+        print(n)
