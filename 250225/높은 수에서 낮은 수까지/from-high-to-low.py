19c8149ab221cf59bbf811b724d6ef0549b30e46
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-02-25 02:16:54 +0900

    Title: 높은 수에서 낮은 수까지, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250225/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 250225/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,8 +1,8 @@
 A, B = map(int, input().split())
 
 if A > B :
-    for i in range(B, A+1, 1):
+    for i in range(A, B-1, -1):
         print(i, end=" ")
 else :
-    for j in range(A, B+1, 1):
+    for j in range(B, A-1, -1):
         print(j, end=" ")
