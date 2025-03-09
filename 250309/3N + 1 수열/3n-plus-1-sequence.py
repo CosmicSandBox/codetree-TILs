09a349fe33de062824c979116583920d3eeb0d70
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-03-09 22:05:22 +0900

    Title: 3N + 1 수열, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/3N + 1 수열/3n-plus-1-sequence.py
+++ 250309/3N + 1 수열/3n-plus-1-sequence.py
@@ -9,8 +9,10 @@ while True :
         if N == 1:
             print(cnt)
             break
+    elif N == 1:
+        print(0)
+        break
     else :
-        N % 2 != 0 :
         N = N*3 + 1
         cnt += 1
         if N == 1:
