e30a461f7499548352070f2c7fd399c124d0e7ab
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-03-10 11:29:37 +0900

    Title: 조건에 따른 연산, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250310/조건에 따른 연산/operatino-by-rule.py
+++ 250310/조건에 따른 연산/operatino-by-rule.py
@@ -4,13 +4,13 @@ cnt = 0
 
 while True :
     if N % 2 == 0 :
-        N*3 + 1
+        N = N*3 + 1
         cnt += 1
         if N >= 1000:
             print(cnt)
             break
     elif N % 2 != 0:
-        N*2 + 2
+        N = N*2 + 2
         cnt += 1
         if N >= 1000:
             print(cnt)
