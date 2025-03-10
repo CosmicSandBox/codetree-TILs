0bf3f9c4ce4001fecb49ecc3f7ca0f99026f4020
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-03-10 11:36:10 +0900

    Title: ab 사이에 있는 c, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250310/ab 사이에 있는 c/c-between-a-and-b.py
+++ 250310/ab 사이에 있는 c/c-between-a-and-b.py
@@ -1,6 +1,13 @@
 a, b, c = map(int,input().split())
 
-if c >= a and c <= b :
+satisfied = True
+for i in range (a, b+1):
+    if i%c == 0 and i >= a and c <= b :
+        satisfied = True
+    else :
+        satisfied = False
+
+if satisfied == True :
     print("YES")
 else :
     print("NO")
