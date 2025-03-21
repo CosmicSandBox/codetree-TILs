589fc61013113dcd5a821814301bd020ce73896c
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-03-21 16:33:32 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250321/특정 규칙에 따른 문자 출력/character-output-according-to-rule.py
+++ 250321/특정 규칙에 따른 문자 출력/character-output-according-to-rule.py
@@ -26,7 +26,7 @@ N = int(input())
 
 for i in range (N): # 0, 1, 2
     for j in range (N-i):
-        print("  ",end="")
+        print(" ",end="")
     for k in range (i+1):
         print("@",end="")
     print()
@@ -34,6 +34,6 @@ for l in range (N-1): # 0, 1
     for m in range (N-2-(l+1)):
         print("@",end="")
     for n in range (l+1):
-        print("  ",end="")
+        print(" ",end="")
     print()
     
