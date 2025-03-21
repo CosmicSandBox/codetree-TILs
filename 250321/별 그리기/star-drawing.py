48dc98e75bfc5f94174c4902ec8431ad47a2f7cb
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-03-21 16:13:59 +0900

    Title: 별 그리기, Time: 71ms, Memory: 18MB, Status: Passed - CodeTree

--- 250321/별 그리기/star-drawing.py
+++ 250321/별 그리기/star-drawing.py
@@ -7,22 +7,30 @@ N = 3
 별의 갯수 : 1, 3, 5,/ 3, 1
 공백 갯수 : 2, 1, 0,/ 1, 2
 
+N = 4
+별의 갯수 : 1, 3, 5, 7 / 5, 3, 1 (지금 4 2 0 이렇게 뜬다.)
+공백 갯수 : 3, 2, 1, 0 / 1, 2, 3
+
+N = 5
+별의 갯수 : 1, 3, 5, 7, 9 / 7, 5, 3, 1
+공백 갯수 : 4, 3, 2, 1, 0 / 1, 2, 3, 4
+
 """
 
 
 
 N = int(input())
 
-for i in range (N): # 0, 1, 2
+for i in range (N): # 0, 1, 2, 3
     for j in range(N-1-i):
         print(" ",end="")
     for k in range(1+2*i):
         print("*",end="")
     print()
 
-for l in range (N-1): # 0, 1
+for l in range (N-1): # 0, 1, 2
     for m in range (l+1):
         print(" ",end="")
-    for n in range(N-2*l):
+    for n in range(2*N-3-2*l): # 5, 3, 1
         print("*",end="")
     print()
