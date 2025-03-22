9a85014c111416b826def3ed063225328469695c
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-03-22 22:58:38 +0900

    Title: 별표 출력하기 10, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250322/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,29 @@
+N = int(input())
+temp = N
+temp_a = int(N-(N/2-1))
+for i in range (1,N+1,1): # 1, 2, 3, 4, 5
+    if i % 2 != 0 :
+        a=(i+1)//2
+        for j in range(a):
+            print("*",end=" ")
+        print()
+    else :
+        for k in range(temp):
+            print("*", end=" ")
+        temp -= 1
+        print()
+
+for i in range (N,0,-1): # 1, 2, 3, 4, 5
+    if i % 2 != 0 :
+        a=(i+1)//2
+        for j in range(a):
+            print("*",end=" ")
+        print()
+    else :
+        for k in range(temp_a+1):
+            print("*", end=" ")
+        temp_a += 1
+        print()
+
+
+    
