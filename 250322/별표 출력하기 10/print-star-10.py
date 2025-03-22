29643750aa701f8a04dc64484ba61b284201fc63
Kimjaewoo <96773327+CosmicSandBox@users.noreply.github.com>
2025-03-22 22:59:35 +0900

    Title: 별표 출력하기 10, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250322/별표 출력하기 10/print-star-10.py
+++ 250322/별표 출력하기 10/print-star-10.py
@@ -1,6 +1,9 @@
 N = int(input())
 temp = N
-temp_a = int(N-(N/2-1))
+if N%2 != 0:
+    temp_a = int(N-(N/2-1))
+else :
+    temp_a = int(N-(N/2))
 for i in range (1,N+1,1): # 1, 2, 3, 4, 5
     if i % 2 != 0 :
         a=(i+1)//2
